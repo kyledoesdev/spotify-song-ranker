b25ad7dd328b41dfb0f653f40563067aa9f0a653
Kyle Evangelisto <kyleevangelisto@gmail.com>
2024-02-24 17:13:46 -0500

    fix pathing to logo

--- resources/views/rank/show.blade.php
+++ resources/views/rank/show.blade.php
@@ -200,7 +200,7 @@
                     style="border-bottom: 2px solid #06D6A0; padding-bottom: 5px;"
                 >
                     <p style="display: inline; color: #06D6A0;">
-                        Listen on <img src="/spotify-logo.png" style="display: inline;">
+                        Listen on <img src="/public/spotify-logo.png" style="display: inline;">
                     </p>
                     <div style="display: inline-block; width: 5px;"></div>
                     <i class="fa-solid fa-arrow-up-right-from-square"></i>
