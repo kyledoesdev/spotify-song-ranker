b820fac05c8b9d708668ab852a565460b4d648b5
Shift <shift@laravelshift.com>
2024-06-04 22:51:53 +0000

    Shift cleanup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-
     protected function setUp(): void
     {
         parent::setUp();
