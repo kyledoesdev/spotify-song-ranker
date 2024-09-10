d2e84283cb73909eac1be8547f7cb496747523eb
kylenotfound <kyleonline6@proton.me>
2024-09-09 21:53:42 -0400

    fucking hard coded url my b

--- resources/js/components/Explore/Points/Creator.vue
+++ resources/js/components/Explore/Points/Creator.vue
@@ -18,7 +18,7 @@
 
         computed: {
             getProfileURL() {
-                return '/ranks?user=' + this.ranking.user.spotify_id;
+                return '/profile?user=' + this.ranking.user.spotify_id;
             }
         }
     }
