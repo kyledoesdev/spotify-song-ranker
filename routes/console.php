5a04999fbab881c5e12df618c5f429d41c9dc8f3
kyledoesdev <kyleonline6@proton.me>
2024-11-06 21:33:58 -0500

    add backup package

--- routes/console.php
+++ routes/console.php
@@ -1,1 +1,16 @@
 <?php
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('backup:clean')->daily()->at('12:00');
+Schedule::command('backup:run')
+    ->timezone('America/New_York')
+    ->daily()
+    ->at('12:05')
+    ->onFailure(function () {
+        Log::error("BACK UP FAILED WEE WOO WEE WOO");
+    })
+    ->onSuccess(function () {
+        Log::warning("BACK UP SUCCESS");
+    });
