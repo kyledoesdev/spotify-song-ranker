3f58548120ff28c5ca359c47d4fe8520ea078aee
kyledoesdev <kyledoesdev@gmail.com>
2025-08-03 15:33:26 -0400

    fix artist slide show

--- resources/views/livewire/welcome/artist-slideshow.blade.php
+++ resources/views/livewire/welcome/artist-slideshow.blade.php
@@ -1,7 +1,7 @@
 <div class="overflow-hidden relative w-full h-auto mx-auto p-4">    
     <div class="w-full overflow-hidden">
         <div class="flex" 
-             style="animation: artistSlideLeft 60s linear infinite; width: {{ count($this->artists) * 144 }}px; will-change: transform;">
+             style="animation: artistSlideLeft 5s linear infinite; width: {{ count($this->artists) * 144 }}px; will-change: transform;">
             @foreach($this->artists as $index => $artist)
                 <div class="flex-shrink-0 px-2" style="width: 144px;">
                     <img 
