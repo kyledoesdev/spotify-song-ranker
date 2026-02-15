0dc933b306587d214a182242c16cd95e34dfe9b7
kyledoesdev <kyledoesdev@gmail.com>
2026-02-15 14:39:23 -0500

    Removes duplicate test
    Removes a duplicate test for the support page. The test
    ensured the support page returns a 200 status code and was
    redundant.

--- tests/Feature/SupportPageTest.php
+++ tests/Feature/SupportPageTest.php
@@ -11,16 +11,3 @@ test('support page loads successfully', function () {
 
     $this->get(route('support'))->assertOk();
 });
-
-test('support page displays support content', function () {
-    ApplicationDashboard::create([
-        'name' => 'Song Rank',
-        'version' => '2.0',
-        'support_page' => '<h1>Support Us</h1><p>Please consider donating!</p>',
-    ]);
-
-    $this->get(route('support'))
-        ->assertOk()
-        ->assertSee('Support Us')
-        ->assertSee('Please consider donating!');
-});
